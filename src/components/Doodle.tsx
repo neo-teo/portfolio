d16a468ed3fa941861d85f16ed4c68ea23c0b075
teo-clone <theodore.tsivranidis@gmail.com>
2024-04-02 17:14:50 -0700

    maybe solved doodle flickering on hover

--- src/components/Doodle.tsx
+++ src/components/Doodle.tsx
@@ -11,7 +11,7 @@ export default function Doodle() {
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
     >
-        <object data={"/angry_blob.svg"} type="image/svg+xml" width="50px" className={isHovered ? '' : 'hidden'} />
-        <object data={"/blob.svg"} type="image/svg+xml" width="50px" className={isHovered ? 'hidden' : ''} />
+        <object data={"/angry_blob.svg"} type="image/svg+xml" width="50px" className={`absolute z-10 ${isHovered ? '' : 'hidden'}`} />
+        <object data={"/blob.svg"} type="image/svg+xml" width="50px" />
     </div>
 }
