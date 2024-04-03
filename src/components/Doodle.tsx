78538df2cfd2cee0b2a72653ee5dee2fb0b37b94
teo-clone <theodore.tsivranidis@gmail.com>
2024-04-02 17:44:49 -0700

    simplified doodle code

--- src/components/Doodle.tsx
+++ src/components/Doodle.tsx
@@ -5,13 +5,12 @@ import * as React from "react"
 import { useState } from "react";
 
 export default function Doodle() {
-    const [isHovered, setIsHovered] = useState(false);
+    const [isAngry, setIsAngry] = useState(false);
 
     return <div
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
+        onMouseEnter={() => setIsAngry(true)}
+        onMouseLeave={() => setIsAngry(false)}
     >
-        <object data={"/angry_blob.svg"} type="image/svg+xml" width="50px" className={`absolute z-10 ${isHovered ? '' : 'hidden'}`} />
-        <object data={"/blob.svg"} type="image/svg+xml" width="50px" />
+        <object data={isAngry ? "/angry_blob.svg" : "/blob.svg"} type="image/svg+xml" width="50px" />
     </div>
 }
