3fd3135a416a26181a4a5ff5930e8e7302165a0f
teo-clone <theodore.tsivranidis@gmail.com>
2024-04-09 10:36:05 -0400

    replaced myself at bottom with skater

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,18 +2,19 @@ import { commitDateTime } from "@/commitDate"
 import Image from "next/image"
 import * as React from "react"
 import me from "@/../public/me.png"
+import skater from "@/../public/skater.gif"
 
 export default function Footer() {
     return <div className="flex flex-col justify-center items-center pb-[40px] gap-5 text-sm">
 
-        <div className="relative">
+        {/* <div className="relative">
             <Image src={me} className="w-48" alt="Photo of myself" />
             <div className="absolute top-2/3 transform z-[-10] flex flex-col">
                 <div>----------------------------</div>
                 <div>{"| $$$$$$$$$$$$$$$$$$$ |"}</div>
                 <div>----------------------------</div>
             </div>
-        </div>
+        </div> */}
 
         <div className={"flex flex-col items-center gap-2"}>
             <div>
@@ -28,5 +29,7 @@ export default function Footer() {
         <div>
             Last updated: {commitDateTime}
         </div>
-    </div>
+
+        <Image src={skater} className="w-24" alt="skater gif made on ps2nostalgia.win" />
+    </div >
 }
