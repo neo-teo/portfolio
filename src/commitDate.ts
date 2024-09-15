f319871ca11cd0db75615640976e0f8bf69c1773
teo-clone <theodore.tsivranidis@gmail.com>
2024-09-15 03:28:36 -0400

    uncluttering footer

--- src/commitDate.ts
+++ src/commitDate.ts
@@ -1,2 +1,2 @@
 // This file is auto-generated on pre-commit hook
-export const commitDateTime = '09/15/2024, 03:21:36 AM EDT';
+export const commitDateTime = '09/15/2024, 03:28:37 AM EDT';
--- src/components/Doodle.tsx
+++ src/components/Doodle.tsx
@@ -1,16 +1,11 @@
 import * as React from "react"
 
 export default function Doodle() {
-    return <div className="fixed bottom-[12px] right-[10px] flex justify-end px-10">
-        <div className="relative">
-            <object
-                data={"/angry_blob.svg"} type="image/svg+xml" width="100px"
-                className={`absolute z-10 opacity-0 hover:opacity-100`}
-            />
-            <object data={"/blob.svg"} type="image/svg+xml" width="100px" />
-            <div className="absolute top-[91%] w-[300px] transform z-[-10] flex flex-col">
-                <div>{"||&&&&&&&&&||"}</div>
-            </div>
-        </div>
+    return <div className="fixed bottom-[-25px] right-[10px] flex justify-end px-10">
+        <object
+            data={"/angry_blob.svg"} type="image/svg+xml" width="100px"
+            className={`absolute z-10 opacity-0 hover:opacity-100`}
+        />
+        <object data={"/blob.svg"} type="image/svg+xml" width="100px" />
     </div>
 }
